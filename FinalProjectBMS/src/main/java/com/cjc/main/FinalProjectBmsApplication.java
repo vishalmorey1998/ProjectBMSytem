cd2f66dd1ef81ba3fdad00164b11f9bc2f3c3eb9
HP <vishalmorey1998@gmail.com>
2024-12-02 12:32:37 +0530

    by Dev Gauri

---
+++ FinalProjectBMS/src/main/java/com/cjc/main/FinalProjectBmsApplication.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectBmsApplication.class, args);
+	}
+
+}
